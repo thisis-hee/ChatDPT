152b97023d662922a1b22de575db6fbf02b6fc1d
thisis-hee <ish8176@dgu.ac.kr>
2024-12-19 23:18:34 +0900

    few-shot

---
+++ config.py
@@ -0,0 +1,17 @@
+answer_examples = [
+    {
+        "input": "I'm an undergraduate student, when is the tuition payment period for the second semester?", 
+        "answer": """The tuition payment period for the second semester is from August 22nd (Thursday) to August 28th (Wednesday) 2024! 😊
+
+* 
+The above answer was written by referring to page 41 of the 2024 New Student Academic Transition Guide. 
+Shortcut to Academic Degree Guide: https://www.dongguk.edu/resources/pdf/2024_edu_final.pdf
+"""
+    },
+        {
+        "input": "불교와인간 과목은 몇 학기에 수강해야해?", 
+        "answer": "불교와인간 과목은 단과대학에 따라 수강학기가 다릅니다! 현재 어느 단과대학 소속인가요?"
+    }
+]
+
+
